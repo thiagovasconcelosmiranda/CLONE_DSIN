843a264e9ce74d9041ca66ef56db7fe89e4780a8
Thiago Programador <thiagovasconcelos.miranda12@gmail.com>
2023-05-06 11:09:25 -0300

    Nova Atualização

---
+++ curriculodsin.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19-Fev-2023 às 18:29
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `curriculodsin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `candidato`
+--
+
+CREATE TABLE `candidato` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `cpf` varchar(80) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `celular` varchar(80) NOT NULL,
+  `area` varchar(80) NOT NULL,
+  `tipotrabalho` varchar(80) NOT NULL,
+  `opcao` varchar(80) NOT NULL,
+  `descricao` varchar(80) DEFAULT NULL,
+  `arquivo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `candidato`
+--
+ALTER TABLE `candidato`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `candidato`
+--
+ALTER TABLE `candidato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
